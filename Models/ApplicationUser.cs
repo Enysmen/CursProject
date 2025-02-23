3f54eab9f436001ac52759a155922da47b108785
EsnemyC <120020955+Enysmen@users.noreply.github.com>
2025-02-23 17:37:39 +0200

    Add
    Add

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CursProject.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;   
+
+        public bool IsBlocked { get; set; } = false;
+
+        public DateTime? LastLoginTime { get; set; }
+
+        public string ThemePreference { get; set; } = "light";
+
+
+    }
+}
